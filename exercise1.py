ec67c0e9d6cc4eac2fcd3d8d009a80f98ac668ce
diony <dsuney91@gmail.com>
2025-02-02 20:12:04 -0500

    adicione un item a items 16 y 17

--- exercise1.py
+++ exercise1.py
@@ -2,7 +2,7 @@ frutas = {"orange", "strawberry", "apple"," lemon"}
 print(frutas)
 print(len(frutas))
 set1 = {"apple", "banana", "cherry","mango"}
-set2 = {1, 5, 7, 9, 3}
+set2 = {1, 5, 7, 9, 3,"banana"}
 set3 = {True, False, False}
 print(set2)
 print(set1)
@@ -13,3 +13,5 @@ print(set4)
 print(len(set4))
 set5 = set1.union(set2)
 print(set5)
+set6 = set1.intersection(set2)
+print(set6)
