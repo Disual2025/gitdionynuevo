7ea5659fe14725511766e1a040bdd4a7246a0e52
diony <dsuney91@gmail.com>
2025-02-02 20:17:42 -0500

    cambie el nombre de item 4 y los demas algoritmos

--- exercise1.py
+++ exercise1.py
@@ -1,17 +1,17 @@
 frutas = {"orange", "strawberry", "apple"," lemon"}
 print(frutas)
 print(len(frutas))
-set1 = {"apple", "banana", "cherry","mango"}
+frutas1 = {"apple", "banana", "cherry","mango"}
 set2 = {1, 5, 7, 9, 3,"banana"}
 set3 = {True, False, False}
 print(set2)
-print(set1)
+print(frutas1)
 print(set3)
 print(len(set3))
-set4 = set1.union(set3)
+set4 = frutas1.union(set3)
 print(set4)
 print(len(set4))
-set5 = set1.union(set2)
+set5 = frutas1.union(set2)
 print(set5)
-set6 = set1.intersection(set2)
+set6 = frutas1.intersection(set2)
 print(set6)
